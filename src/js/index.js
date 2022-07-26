1d928b5a56bd07f43b58aa149131534e500e1c5e
yilin-kim <irenek91@korea.ac.kr>
2022-07-27 01:43:28 +0900

    step2 요구사항 분석

--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,28 @@
 // - [x] 사용자가 확인 버튼을 누르면 메뉴가 삭제된다.
 // - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
 
+// step2 요구사항 분석
+// localStorage Read & Write
+// - [ ] localStorage에 메뉴를 저장한다.
+// - [ ] localStorage에서 메뉴를 가져온다.
+
+// 카테고리별 메뉴판 관리
+// - [ ] 에스프레소 메뉴판 관리
+// - [ ] 프라푸치노 메뉴판 관리
+// - [ ] 블렌디드 메뉴판 관리
+// - [ ] 티바나 메뉴판 관리
+// - [ ] 디저트 메뉴판 관리
+
+// 페이지 접근시 최초 데이터 Read & Rendering
+// - [ ] 페이지에 최초로 접근할 때는 localStorage에서 에스프레소 메뉴를 읽어온다.
+// - [ ] 에스프레소 메뉴를 페이지에 그려준다.
+
+// 품절 상태 관리
+// - [ ] 품절 처리 버튼을 추가한다.
+// - [ ] 품절 버튼을 누르면 localStorage에 품절 상태를 저장한다.
+// - [ ] localStorage에 품절 상태를 저장한다.
+// - [ ] 품절 상태를 UI에 반영한다.
+
 // JS에서 DOM element 가져올 때 관용적으로 $표시를 사용한다.
 // $표시로 DOM element return해서 반복 줄이는 함수
 const $ = (selector) => document.querySelector(selector);
